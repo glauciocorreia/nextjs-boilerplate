28dd88320febd0518b81598324518a445801b09d
glauciocorreia <glauciocorreiaa@gmail.com>
2022-08-16 17:25:53 -0300

    chore(project): update all packages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const runtimeCaching = require('next-pwa/cache')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
     disable: !isProd,
+    dest: 'public',
+    runtimeCaching,
+    register: false,
+    buildExcludes: [
+      /middleware-manifest\.json$/,
+      /_middleware\.js$/,
+      /_middleware\.js\.map$/,
+      /middleware-runtime\.js$/,
+      /middleware-runtime\.js\.map$/,
+    ],
   },
   swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
 })
